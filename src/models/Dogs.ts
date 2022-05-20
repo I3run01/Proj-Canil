7871f003c86aa613a6d6446d8413c5d0b100221c
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-05-20 08:50:47 -0300

    commit

--- src/models/Dogs.ts
+++ src/models/Dogs.ts
@@ -13,5 +13,21 @@ class Dog {
 }
 
 let DogsList = [
-    new Dog('sd', 'Pastor Alemão', 'Amarelo e Preto', 'Masculino')
+    new Dog('images', 'Pastor Alemão', 'Amarelo e Preto', 'Masculino'),
+    new Dog('images', 'Labrador-retriever', 'Branco', 'Masculino'),
+    new Dog('images', 'Zwergspitz', 'Amarelo', 'Masculino'),
+    new Dog('images', 'Husky Siberiano', 'Branco e Preto', 'Masculino'),
+    new Dog('images', 'Golden Retriever', 'Amarelo', 'Masculino'),
+    new Dog('images', 'Poodle', 'Amarelo ', 'Masculino'),
+    new Dog('images', 'Bulldog', 'Amarelo ', 'Masculino'),
+    new Dog('images', 'Persa', 'Amarelo ', 'Masculino'),
+    new Dog('images', 'Maine Coon', 'Amarelo ', 'Masculino'),
+    new Dog('images', 'Bengal', 'Amarelo ', 'Masculino'),
+    new Dog('images', 'Siamês', 'Amarelo ', 'Masculino'),
+    new Dog('images', 'Sphynx', 'Amarelo ', 'Masculino'),
+    new Dog('images', 'Tetra Neon', 'Amarelo ', 'Masculino'),
+    new Dog('images', 'Mato Grosso', 'Amarelo ', 'Masculino'),
+    new Dog('images', 'Limpa Vidro', 'Amarelo ', 'Masculino'),
+    new Dog('images', 'Tanictis', 'Amarelo ', 'Masculino'),
+    new Dog('images', 'Acará Bandeira', 'Amarelo ', 'Masculino'),
 ]
